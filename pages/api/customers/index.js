eab60b79961af50a85e3286ef262d99f3c2bd7fb
james23216 <jhmun23216@gmail.com>
2022-06-25 03:33:27 -0700

    again

--- pages/api/customers/index.js
+++ pages/api/customers/index.js
@@ -1,17 +1,9 @@
 import { pool } from "config/db";
 
 export default async function handler(req, res) {
-  let ti = setTimeout(() => {
-    return res.status(500).json({ error: {
-      message: "Request Timeout !. Your Vercel account must be upgraded to the PRO membership"
-    } });
-  }, 4500);
-
   switch (req.method) {
     case "GET":
-      let result = await getCustomers(req, res)
-      clearTimeout(ti);
-      return result;
+      return await getCustomers(req, res);
     case "POST":
       return await saveCustomer(req, res);
     default:
